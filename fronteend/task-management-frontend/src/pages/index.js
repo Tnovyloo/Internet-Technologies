d6ad65856b6005dbf82af860f5a197226bb1a090
Tomasz Nowak <76777800+Tnovyloo@users.noreply.github.com>
2024-10-14 22:01:44 +0200

    Creating new project, because i cannot integrate two routers into nextjs app

---
+++ fronteend/task-management-frontend/src/pages/index.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import localFont from "next/font/local";
+import Head from "next/head";
+import Card from "@/components/card";
+import Icon from "@/components/icon";
+
+export default function Home() {
+  return (
+    <div className="bg-neutralGray-300/50 min-h-[1200px]">
+      
+      <div>He he</div>
+    </div>
+  );
+}
