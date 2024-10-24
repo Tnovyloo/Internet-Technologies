b8cd6ebc185f8f6e04ff1ea2cc0782b04d390bdf
Tomasz Nowak <76777800+Tnovyloo@users.noreply.github.com>
2024-10-24 22:58:16 +0200

    Injecting properly data to Droppable, and Draggable components

--- fronteend/task-management-frontend/src/pages/tasks/index.jsx
+++ fronteend/task-management-frontend/src/pages/tasks/index.jsx
@@ -46,18 +46,35 @@ export const getServerSideProps = (async (context) => {
 })
 
 export default function Home({ data }) {
-  const [tasks, setTasks] = useState(data)
+  const [columns, setColumns] = useState({
+    todo: {
+      name: "TODO",
+      items: data?.todo
+    },
+    inProgress: {
+      name: "W trakcie",
+      items: data?.inProgress
+    },
+    review: {
+      name: "Review",
+      items: data?.review
+    },
+    ended: {
+      name: "Zakończone",
+      items: data?.ended
+    }
+  });
 
   useEffect(() => {
-    // console.log(tasks.todo)
+    console.log("XD", columns.todo.items)
+
   }, [])
 
-  const onDragEnd = (result, columns, setColumns) => {
+  const onDragEnd = (result) => {
     console.log("onDragEnd")
     return;
   }
   
-
   return (
     <div className="bg-neutralGray-300/50 min-h-[1200px]">
       <Head>
@@ -104,7 +121,8 @@ export default function Home({ data }) {
                     // minHeight: 500
                   }}
                 >
-                  <Column title={`TODO`} data={tasks?.todo && tasks?.todo} shadowClass={'geometric-shadow-red'}>
+
+                  <Column title={`TODO`} data={columns.todo.items && columns.todo.items} shadowClass={'geometric-shadow-red'}>
                     <Icon>
                       <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M12 13V7M9 10H15M19 21V7.8C19 6.11984 19 5.27976 18.673 4.63803C18.3854 4.07354 17.9265 3.6146 17.362 3.32698C16.7202 3 15.8802 3 14.2 3H9.8C8.11984 3 7.27976 3 6.63803 3.32698C6.07354 3.6146 5.6146 4.07354 5.32698 4.63803C5 5.27976 5 6.11984 5 7.8V21L12 17L19 21Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
@@ -114,50 +132,110 @@ export default function Home({ data }) {
 
                   {provided.placeholder}
                 </div>
-            );
-          }}
+              );
+            }}
 
           </Droppable>
 
-          {/* <Column title={`TODO`} data={tasks?.todo && tasks?.todo} shadowClass={'geometric-shadow-red'}>
-            <Icon>
-              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M12 13V7M9 10H15M19 21V7.8C19 6.11984 19 5.27976 18.673 4.63803C18.3854 4.07354 17.9265 3.6146 17.362 3.32698C16.7202 3 15.8802 3 14.2 3H9.8C8.11984 3 7.27976 3 6.63803 3.32698C6.07354 3.6146 5.6146 4.07354 5.32698 4.63803C5 5.27976 5 6.11984 5 7.8V21L12 17L19 21Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-              </svg>
-            </Icon>
-          </Column> */}
+
+          <Droppable droppableId="in_work" key="in_work">
+          {(provided, snapshot) => {
+              return (
+                <div
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                  style={{
+                    background: snapshot.isDraggingOver
+                      ? "lightblue"
+                      : "lightgrey",
+                    padding: 4,
+                    // width: 250,
+                    // minHeight: 500
+                  }}
+                >
+                  <Column title={`W trakcie`} data={columns?.inProgress.items && columns?.inProgress.items} shadowClass={`geometric-shadow-gray`}>
+                    <Icon>
+                      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M17 17L22 12L17 7M7 7L2 12L7 17M14 3L10 21" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                      </svg>
+
+                    </Icon>
+                  </Column>
+
+                  {provided.placeholder}
+                </div>
+              );
+            }}
+
+          </Droppable>
+
+          <Droppable droppableId="review" key="review">
+          {(provided, snapshot) => {
+              return (
+                <div
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                  style={{
+                    background: snapshot.isDraggingOver
+                      ? "lightblue"
+                      : "lightgrey",
+                    padding: 4,
+                    // width: 250,
+                    // minHeight: 500
+                  }}
+                >
+                  <Column title={`Review`} data={columns?.review.items && columns?.review.items} shadowClass={`geometric-shadow-gray`}>
+                    <Icon>
+                      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12 15.5H7.5C6.10444 15.5 5.40665 15.5 4.83886 15.6722C3.56045 16.06 2.56004 17.0605 2.17224 18.3389C2 18.9067 2 19.6044 2 21M16 18L18 20L22 16M14.5 7.5C14.5 9.98528 12.4853 12 10 12C7.51472 12 5.5 9.98528 5.5 7.5C5.5 5.01472 7.51472 3 10 3C12.4853 3 14.5 5.01472 14.5 7.5Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                      </svg>
+                    </Icon>
+
+                  </Column>
+
+                  {provided.placeholder}
+                </div>
+              );
+            }}
+
+          </Droppable>
 
 
-          <Column title={`W trakcie`} data={tasks?.in_work && tasks?.in_work} shadowClass={`geometric-shadow-gray`}>
-            <Icon>
-              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M17 17L22 12L17 7M7 7L2 12L7 17M14 3L10 21" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-              </svg>
+          <Droppable droppableId="ended" key="ended">
+          {(provided, snapshot) => {
+              return (
+                <div
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                  style={{
+                    background: snapshot.isDraggingOver
+                      ? "lightblue"
+                      : "lightgrey",
+                    padding: 4,
+                    // width: 250,
+                    // minHeight: 500
+                  }}
+                >
+                  <Column title={`Zakończone`} data={columns?.ended.items && columns?.ended.items} shadowClass={`geometric-shadow-green`}>
+                    <Icon key={'svg'}>
+                      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M7.5 12L10.5 15L16.5 9M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                      </svg>
+                    </Icon>
 
-            </Icon>
-          </Column>
+                    <div key={'div'}>
+                      Helllllo
+                    </div>
+                  </Column>
 
+                  {provided.placeholder}
+                </div>
+              );
+            }}
 
-          <Column title={`Review`} data={tasks?.review && tasks?.review} shadowClass={`geometric-shadow-gray`}>
-            <Icon>
-              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M12 15.5H7.5C6.10444 15.5 5.40665 15.5 4.83886 15.6722C3.56045 16.06 2.56004 17.0605 2.17224 18.3389C2 18.9067 2 19.6044 2 21M16 18L18 20L22 16M14.5 7.5C14.5 9.98528 12.4853 12 10 12C7.51472 12 5.5 9.98528 5.5 7.5C5.5 5.01472 7.51472 3 10 3C12.4853 3 14.5 5.01472 14.5 7.5Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-              </svg>
-            </Icon>
+          </Droppable>
+          
 
-          </Column>
-
-          <Column title={`Zakończone`} data={tasks?.review && tasks?.review} shadowClass={`geometric-shadow-green`}>
-            <Icon key={'svg'}>
-              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M7.5 12L10.5 15L16.5 9M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-              </svg>
-            </Icon>
-
-            <div key={'div'}>
-              Helllllo
-            </div>
-          </Column>
         </div>
 
       </DragDropContext>
